688454a8e47707dad2ef50e3759c05bf967c401a
test <test@mail.cn>
2022-11-08 14:06:12 +0800

    调整保存结果

--- main.py
+++ main.py
@@ -17,28 +17,21 @@ def get_vmess():
         vmess_array.add(f)
 
 
-# def test():
-#     s = 'vmess://eyJhZGQiOiIyNS43MmltZy54eXoiLCJwcyI6IvCfh7rwn4e4X1VTX+e+juWbvS0+8J+HsPCfh7dfS1Jf6Z+p5Zu9Iiwic2N5IjoiYXV0byIsInByb3h5VHlwZSI6InZtZXNzIiwidHlwZSI6Im5vbmUiLCJzbmkiOiIiLCJwYXRoIjoiLyIsInBvcnQiOjQ0MywidiI6MiwiaG9zdCI6IiIsInRscyI6InRscyIsImlkIjoiODFkOTNmNjItMTVhMi00OTk0LWFkYjktMGI1ZDkwNmFhYzdlIiwibmV0Ijoid3MiLCJhaWQiOjAsIm9yaWdpbk5hbWUiOiJSZWxheV/wn4e68J+HuFVTLfCfh7Pwn4exTkxfMTA5NSJ9'
-#     config = json.loads(base64.b64decode(s.split('//')[1]).decode('utf-8'))
-#     print(config)
-#     response = ping(config['add'])
-#     if response is not None:
-#         delay = int(response * 1000)
-#         print(delay, "延迟")
-
-
-def delay(address):
-    s = 'vmess://eyJhZGQiOiIyNS43MmltZy54eXoiLCJwcyI6IvCfh7rwn4e4X1VTX+e+juWbvS0+8J+HsPCfh7dfS1Jf6Z+p5Zu9Iiwic2N5IjoiYXV0byIsInByb3h5VHlwZSI6InZtZXNzIiwidHlwZSI6Im5vbmUiLCJzbmkiOiIiLCJwYXRoIjoiLyIsInBvcnQiOjQ0MywidiI6MiwiaG9zdCI6IiIsInRscyI6InRscyIsImlkIjoiODFkOTNmNjItMTVhMi00OTk0LWFkYjktMGI1ZDkwNmFhYzdlIiwibmV0Ijoid3MiLCJhaWQiOjAsIm9yaWdpbk5hbWUiOiJSZWxheV/wn4e68J+HuFVTLfCfh7Pwn4exTkxfMTA5NSJ9'
-    config = json.loads(base64.b64decode(s.split('//')[1]).decode('utf-8'))
-    print(config)
-    response = ping(config['add'])
-    if response is not None:
-        delay = int(response * 1000)
-        print(delay, "延迟")
+def delay():
+    try:
+        for s in vmess_array:
+            config = json.loads(base64.b64decode(s.split('//')[1]).decode('utf-8'))
+            response = ping(config['add'])
+            if response is not None:
+                delay_ = int(response * 1000)
+                print(config['ps'], " 延迟", delay_)
+    except Exception as e:
+        print()
 
 
 if __name__ == '__main__':
     get_vmess()
-    encode = base64.b64encode(vmess_array.__str__().encode("utf-8")).decode('utf-8')
+    encode = base64.b64encode('\r\n'.join(vmess_array).encode("utf-8")).decode('utf-8')
     v = open('v', 'w', encoding='UTF-8')
     v.write(encode)
+    delay()
