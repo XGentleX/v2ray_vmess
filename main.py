df3b2d78424d43007b88890045a168fa4f696196
test <test@mail.cn>
2022-11-12 11:49:03 +0800

    update

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import base64
 from modules import a, b, c
 
 vmess_array = set(())
+schemes_allow = ['vmess', 'ss', 'socks', 'ssr', 'trojan']
 
 
 # noinspection PyBroadException
@@ -26,6 +27,13 @@ def get():
 
 if __name__ == '__main__':
     get()
-    encode = base64.b64encode('\r\n'.join(vmess_array).encode("utf-8")).decode('utf-8')
+    link_array = set()
+    for link in vmess_array:
+        segmentation_array = link.split('://')
+        if segmentation_array.__len__() <= 0:
+            continue
+        if segmentation_array[0] in schemes_allow:
+            link_array.add(link)
+    encode = base64.b64encode('\r\n'.join(link_array).encode("utf-8")).decode('utf-8')
     v = open('v', 'w', encoding='UTF-8')
     v.write(encode)
